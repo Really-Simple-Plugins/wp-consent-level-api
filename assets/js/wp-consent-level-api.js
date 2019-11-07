ceb4d5895f13481543c219b079e669022eea7bc2
Rogier Lankhorst <rogier@really-simple-plugins.com>
2019-11-07 20:37:03 +0100

    set cookie js

--- assets/js/wp-consent-level-api.js
+++ assets/js/wp-consent-level-api.js
@@ -30,8 +30,13 @@ jQuery(document).ready(function ($) {
      */
 
     function wpProcessConsentLevelChange(consentLevels) {
-        //set the changed content levels in wp cookies
         console.log(consentLevels);
+        //foreach consent level, set the value in a cookie
+        for (var key in consentLevels) {
+            if (consentLevels.hasOwnProperty(key)) {
+                clapiSetCookie('wp_consent_'+key, consentLevels[key][0], cookie_expiry);
+            }
+        }
 
         //trigger a hook for plugins to hook into
         $.event.trigger({
@@ -48,6 +53,35 @@ jQuery(document).ready(function ($) {
 
     function wpHasConsentLevel(){
 
+    }
+
+
+    function clapiSetCookie(name, value, days) {
+        var secure = ";secure";
+
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = ";expires=" + date.toGMTString();
+
+        if (window.location.protocol !== "https:") secure = '';
+
+        document.cookie = name + "=" + value + secure + expires + ";path=/";
+    }
+
+    function clapiGetCookie(cname) {
+        var name = cname + "="; //Create the cookie name variable with cookie name concatenate with = sign
+        var cArr = window.document.cookie.split(';'); //Create cookie array by split the cookie by ';'
+
+        //Loop through the cookies and return the cooki value if it find the cookie name
+        for (var i = 0; i < cArr.length; i++) {
+            var c = cArr[i].trim();
+            //If the name is the cookie string at position 0, we found the cookie and return the cookie value
+            if (c.indexOf(name) == 0)
+                return c.substring(name.length, c.length);
+        }
+
+        //If we get to this point, that means the cookie wasn't found, we return an empty string.
+        return "";
     }
 
 });
