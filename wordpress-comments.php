51611598d0d1a569df48a48ff59104179dc488a4
mrxkon <mr.xkon@gmail.com>
2020-01-28 22:55:55 +0200

    Update wordpress-comments.php

--- wordpress-comments.php
+++ wordpress-comments.php
@@ -1,47 +1,40 @@
 <?php // phpcs:ignore -- Ignore the  "\r\n" notice for some machines.
 
 /**
- * Instead of listening to the consent checkbox, this function checks for the "preferences" consent
+ * Instead of listening to the consent checkbox, this way we check for the "preferences" consent.
  *
- * The consent checkbox is removed
- * if preferences is set, the comments cookies function gets passed a $cookies_consent=true, so cookies can be set. Otherwise, it's false.
+ * The consent checkbox is removed if preferences is set, the comments cookies function gets
+ * passed a $cookies_consent=true, so cookies can be set. Otherwise, it's false.
  */
 
-/**
- * First we remove the default comment cookies action, and replace with our own
- * we add custom comment cookies action, which checks the consent, then calls the comment cookie function in wp
- */
+add_action(
+	'init',
+	function() {
+		// Remove the default WordPress comment cookies action.
+		remove_action(
+			'set_comment_cookies',
+			'wp_set_comment_cookies',
+			10
+		);
 
-function wp_consent_api_wordpress_comments_cookies() {
+		// Custom action to check the consent level and decide if cookies can be set.
+		add_action(
+			'set_comment_cookies',
+			function( $comment, $user, $cookies_consent ) {
+				$cookies_consent = wp_has_consent( 'preferences' );
+				wp_set_comment_cookies( $comment, $user, $cookies_consent );
+			},
+			10,
+			3
+		);
 
-	// Remove default wp action.
-	remove_action( 'set_comment_cookies', 'wp_set_comment_cookies', 10 );
-
-	// Add our own custom action.
-	add_action( 'set_comment_cookies', 'wp_consent_api_set_comment_cookies', 10, 3 );
-
-	// Remove checkbox.
-	add_filter( 'comment_form_default_fields', 'wp_consent_api_wordpress_comment_form_hide_cookies_consent' );
-
-}
-add_action( 'init', 'wp_consent_api_wordpress_comments_cookies' );
-
-/**
- * Custom consent function, checking consent level to decide if cookies can be set.
- */
-function wp_consent_api_set_comment_cookies( $comment, $user, $cookies_consent ) {
-	$cookies_consent = wp_has_consent( 'preference' );
-	wp_set_comment_cookies( $comment, $user, $cookies_consent );
-}
-
-/**
- * Remove consent checkbox
- * @param $fields
- *
- * @return array $fields
- */
-
-function wp_consent_api_wordpress_comment_form_hide_cookies_consent( $fields ) {
-	unset( $fields['cookies'] );
-	return $fields;
-}
+		// Remove consent checkbox.
+		add_filter(
+			'comment_form_default_fields',
+			function( $fields ) {
+				unset( $fields['cookies'] );
+				return $fields;
+			}
+		);
+	}
+);
