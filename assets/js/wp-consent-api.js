2b032457a0deff9b33cc15cc430ebbb1d193c33c
Ivan Kristianto <ivan.kristianto@gmail.com>
2021-01-12 11:24:27 +0700

    Add additional path param to consent_api_set_cookie
    This additional path param, allows the third party to set custom
    cookie path. This is useful for WordPress with subdirectory mode
    where each site can have it's own consent.

--- assets/js/wp-consent-api.js
+++ assets/js/wp-consent-api.js
@@ -50,10 +50,11 @@ function wp_has_consent(category) {
 /**
  * Set cookie by consent type.
  *
- * @param {string} name The cookie name to set.
+ * @param {string} name  The cookie name to set.
  * @param {string} value The cookie value to set.
+ * @param {string} path  The cookie path to set.
  */
-function consent_api_set_cookie(name, value) {
+function consent_api_set_cookie(name, value, path = '/') {
     var secure = ";secure";
     var days = consent_api.cookie_expiration;
     var date = new Date();
@@ -62,7 +63,7 @@ function consent_api_set_cookie(name, value) {
 
     if (window.location.protocol !== "https:") secure = '';
 
-    document.cookie = name + "=" + value + secure + expires + ";path=/";
+    document.cookie = name + "=" + value + secure + expires + ";path=" + path;
 }
 
 /**
